6038c95b276d196485e214432d922a71d69a73c6
Ethan <99150762+fridays181@users.noreply.github.com>
2022-02-10 11:40:16 -0500

    Create lvl-4.py

---
+++ lvl-4.py
@@ -0,0 +1,16 @@
+#Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+def remove_chars(word, n):
+    print('Original string:', word)
+    x = word[n:]
+    return x
+
+print("Removing characters from a string")
+print(remove_chars("pynative", 4))
+print(remove_chars("pynative", 2))
+
+
+#practice--
+#def etc(x, xx)
+#[1:]
+#return
